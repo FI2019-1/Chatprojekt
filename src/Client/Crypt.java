af5a635a818775eb601efdc5749056fafcea37c8
Yannick-25 <74174739+Yannick-25@users.noreply.github.com>
2021-06-30 22:01:11 +0200

    Create Crypt.java
    Klasse Crypt (Verschlüsselungsklasse) hinzugefügt

---
+++ src/Client/Crypt.java
@@ -0,0 +1,88 @@
+package Client;
+
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+
+public class Crypt
+{
+    private  static final String ALGORITHM = "RSA";
+
+    private static final String TEXT = "Hallo dies ist ein Test!!!";
+
+    PublicKey pubKey;
+    PrivateKey privKey;
+
+    public byte[] encrypt (PublicKey key, byte[] plain) throws  Exception
+    {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        //Verschlüsseln
+
+        return  cipher.doFinal(plain);
+    }
+
+    public byte[] decrypt(PrivateKey key, byte[] chiffr) throws Exception
+    {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+
+        cipher.init(Cipher.DECRYPT_MODE, key);
+
+        //Entschlüsseln
+
+        return cipher.doFinal(chiffr);
+    }
+
+    public void Schluesselpaar()
+    {
+        try
+        {
+            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(ALGORITHM);
+
+            keyPairGen.initialize(1024);
+
+            KeyPair keyPair = keyPairGen.generateKeyPair();
+
+            PrivateKey privKey = keyPair.getPrivate();
+
+            PublicKey pubKey = keyPair.getPublic();
+
+            //Es werden die Bytes vom Text benötigt
+
+            /*byte[] bytes = TEXT.getBytes(); // TEXT ist austauschen gegen unseren String
+
+            byte[] encrypt = encrypt(pubKey, bytes);
+
+            byte[] decrypt = decrypt(privKey, encrypt);*/
+
+            /*System.out.println(encrypt);
+
+            System.out.println("\n");
+
+            System.out.println(decrypt);
+            System.out.println("Verschlüsselt: " + new String(decrypt));
+
+            String ausgabe = new String(decrypt);
+            System.out.println(ausgabe);*/
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public PublicKey getPublicKey()
+    {
+        return pubKey;
+    }
+
+    public PrivateKey getPrivateKey()
+    {
+        return privKey;
+    }
+}
