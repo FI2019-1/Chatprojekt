7d24085651f27cbef9d0f694a101216e50c773ce
Yannick-25 <74174739+Yannick-25@users.noreply.github.com>
2021-06-30 21:58:33 +0200

    Update ClientController.java
    Auskommentierte Verschlüsselungsversuche hinzugefügt

--- src/Client/ClientController.java
+++ src/Client/ClientController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 
+import javax.net.ssl.*;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,6 +23,9 @@ public class ClientController implements Initializable
 
     private String username;
     ClientProxy cp;
+
+    private static final String[] protocols = new String[] {"TLSv1.3"};
+    private static final String[] cipher_suites = new String[] {"TLS_AES_128_GCM_SHA256"};
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
@@ -67,12 +71,47 @@ public class ClientController implements Initializable
         try
         {
             Socket client = new Socket("localhost", 5555);
+            //neu
+            //SSLSocketFactory sslFactory = (SSLSocketFactory)SSLSocketFactory.getDefault();
+            //SSLSocket client = (SSLSocket)sslFactory.createSocket("localhost", 5555);
+            //client.setUseClientMode(true);
+            /*client.setEnabledCipherSuites( new String[] { "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256" });
+            client.setEnabledProtocols(new String[] { "TLSv1.2" });
+            SSLParameters sslParams = new SSLParameters();
+            sslParams.setEndpointIdentificationAlgorithm("HTTPS");
+             client.setSSLParameters(sslParams);
+            //client.startHandshake();*/
+            //client.setEnabledProtocols(protocols);
+            //client.setEnabledCipherSuites(cipher_suites);
+            //client.startHandshake();
+            /*neu 24.06
+            client.addHandshakeCompletedListener(
+                    new HandshakeCompletedListener() {
+                        public void handshakeCompleted(
+                                HandshakeCompletedEvent event) {
+                            System.out.println("Handshake finished!");
+                            System.out.println(
+                                    "\t CipherSuite:" + event.getCipherSuite());
+                            System.out.println(
+                                    "\t SessionId " + event.getSession());
+                            System.out.println(
+                                    "\t PeerHost " + event.getSession().getPeerHost());
+                        }
+                    }
+            );*/
+
+
+            //client.startHandshake();
+            //neu ende
             System.out.println("Client konnte gestartet werden :)");
-            cp = new ClientProxy(client, this);
+            cp = new ClientProxy(client, this); //geht nicht in ClientProxy
             Thread t = new Thread(cp);
             t.start();
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             System.out.println("Fehler in CC starteClient D:");
+            e.printStackTrace();
         }
     }
 
@@ -88,4 +127,6 @@ public class ClientController implements Initializable
         nachrichten.setEditable(true);
         name.setEditable(false);
     }
+
+
 }
